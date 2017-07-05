9798b21857e38c11a54cc663c64da951b7433bfa
leeintae <hbigman0921@naver.com>
2017-07-05 20:55:07 +0900

    Create selection.cpp

---
+++ selection.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+using namespace std;
+
+#define MAX_SIZE 1000000
+
+int arr[MAX_SIZE];
+int N;
+
+void selection_sort()
+{
+	int min_index;
+	int tmp;
+
+	for (int i = 0; i < N - 1; i++) {
+		min_index = i;
+		for (int j = i; j < N; j++) {
+			if (arr[j] < arr[min_index])
+				min_index = j;
+		}
+		tmp = arr[i];
+		arr[i] = arr[min_index];
+		arr[min_index] = tmp;
+	}
+}
+
+int main(void)
+{
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+
+	selection_sort();
+
+	for (int i = 0; i < N; i++)
+		printf("%d\n", arr[i]);
+}
